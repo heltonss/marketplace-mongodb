976ab2b76a5d63490baa658b568de82bea9565d2
heltonss <helton.silveira@grupoltm.com.br>
2019-05-08 10:33:40 -0300

    add - config environment

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,9 +1,9 @@
 module.exports = {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'edb6b730f06e5f',
-    pass: 'afc33f0d1891d7'
+    user: process.env.REDIS_HOST,
+    pass: process.env.MAIL_PORT
   }
 }
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 module.exports = {
-  dsn: 'https://b066280451584d2491f20ffdcd3d176d@sentry.io/1455171'
+  dsn: process.env.SENTRY_DSN
 }
